97707658acc4a1fe5b35c5cb4dfebd7eee82407c
DevDelali <delalpaul5@gmail.com>
2026-01-17 15:48:09 +0000

    learning activities w2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,14 +1,32 @@
-const input = document.getElementById('favchap');
-const button = document.querySelector('button');
-const list = document.getElementById('list');
+if (input.value !== '') {
+    const input = document.getElementById('favchap');
+    const button = document.querySelector('button');
+    const list = document.getElementById('list');
 
-const li = document.createElement('li');
+    const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+    li.textContent = input.value;
 
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+    deleteButton.textContent = '❌';
 
+    li.append(deleteButton);
+
+    list.append(li);
+
+    <button aria-label="Close" id="close-button">❌</button>
+
+    addEventListener('click', function () {
+        li.remove();
+    });
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    input.value = '';
+    input.focus();
+
+}
