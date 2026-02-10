eca85714017b14834a950de23aa9bcc6f184ebd9
DevDelali <delalpaul5@gmail.com>
2026-02-10 10:49:31 +0000

    advanced my from assignment

--- scripts/form.js
+++ scripts/form.js
@@ -26,3 +26,58 @@ if (lastModified) {
 if (currentYear) {
     currentYear.textContent = today.getFullYear();
 }
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+// populate product select options
+const productSelect = document.querySelector("#product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let reviewCount = localStorage.getItem("reviewCount");
+
+    if (reviewCount === null) {
+        reviewCount = 0;
+    }
+
+    reviewCount = Number(reviewCount) + 1;
+
+    localStorage.setItem("reviewCount", reviewCount);
+
+    document.getElementById("reviewCount").textContent = reviewCount;
+
+});
